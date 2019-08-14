4e39c6a7ee13be894af6e1f5291dedb60d12b2ca
Matt Pitner <mattpitner09@gmail.com>
2019-08-14 22:02:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,14 @@
 array = [1,2,3]
 
+
 def square_array(array)
+  array2 = []
   array.each do |number|
-     number * number
+     number * number << array2
+     
   end 
+  
+  return array2
 
   
 end
